a29d44746c281c78ccd1b385c4161c81040e634b
developerSajib88 <developersajib88@gmail.com>
2024-04-16 09:54:36 +0600

    Local database structure create. Api urls implement. Cart, Category and Product model create.

---
+++ lib/models/category/category_model.dart
@@ -0,0 +1,81 @@
+class CategoryModel {
+  final List<Product>? products;
+  final int? total;
+  final int? skip;
+  final int? limit;
+
+  CategoryModel({
+    this.products,
+    this.total,
+    this.skip,
+    this.limit,
+  });
+
+  CategoryModel copyWith({
+    List<Product>? products,
+    int? total,
+    int? skip,
+    int? limit,
+  }) =>
+      CategoryModel(
+        products: products ?? this.products,
+        total: total ?? this.total,
+        skip: skip ?? this.skip,
+        limit: limit ?? this.limit,
+      );
+}
+
+class Product {
+  final int? id;
+  final String? title;
+  final String? description;
+  final int? price;
+  final double? discountPercentage;
+  final double? rating;
+  final int? stock;
+  final String? brand;
+  final String? category;
+  final String? thumbnail;
+  final List<String>? images;
+
+  Product({
+    this.id,
+    this.title,
+    this.description,
+    this.price,
+    this.discountPercentage,
+    this.rating,
+    this.stock,
+    this.brand,
+    this.category,
+    this.thumbnail,
+    this.images,
+  });
+
+  Product copyWith({
+    int? id,
+    String? title,
+    String? description,
+    int? price,
+    double? discountPercentage,
+    double? rating,
+    int? stock,
+    String? brand,
+    String? category,
+    String? thumbnail,
+    List<String>? images,
+  }) =>
+      Product(
+        id: id ?? this.id,
+        title: title ?? this.title,
+        description: description ?? this.description,
+        price: price ?? this.price,
+        discountPercentage: discountPercentage ?? this.discountPercentage,
+        rating: rating ?? this.rating,
+        stock: stock ?? this.stock,
+        brand: brand ?? this.brand,
+        category: category ?? this.category,
+        thumbnail: thumbnail ?? this.thumbnail,
+        images: images ?? this.images,
+      );
+}
